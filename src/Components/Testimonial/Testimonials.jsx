55f8dfd6fa551d5c8637340deec8621af8e4d1f3
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-08-01 22:46:53 +0100

    removed swiper

--- src/Components/Testimonial/Testimonials.jsx
+++ src/Components/Testimonial/Testimonials.jsx
@@ -1,43 +1,25 @@
-import './Testimonials.css'
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import "./Testimonials.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import testimony from './testData';
-
+import testimony from "./testData";
 
 const Testimonials = () => {
   return (
     <section className="container">
       <div className="testimonial-container">
         <div className="testimony">
-          {testimony.map(({id, picture, names, position, statement})=> {
+          {testimony.map(({ id, picture, names, position, statement }) => {
             return (
-              <div className="testimony-container">
-                <Swiper
-                  key={id}
-                  // install Swiper modules
-                  modules={[Navigation, Pagination, Scrollbar, A11y]}
-                  spaceBetween={50}
-                  slidesPerView={1}
-                  navigation
-                  pagination={{ clickable: true }}
-                  scrollbar={{ draggable: true }}
-                >
-                  <SwiperSlide>
-                    <img src={picture} alt="pic" />
-                    <div className="status">
-                      <h3>{names}</h3>
-                      <small>{position}</small>
-                    </div>
-                    <p>{statement}</p>
-                  </SwiperSlide>
-                </Swiper>
+              <div className="testimony-container" key={id}>
+                <img src={picture} alt="pic" />
+                <div className="status">
+                  <h3>{names}</h3>
+                  <small>{position}</small>
+                </div>
+                <p>{statement}</p>
               </div>
             );
           })}
@@ -45,6 +27,6 @@ const Testimonials = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Testimonials
+export default Testimonials;
